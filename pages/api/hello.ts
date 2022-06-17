6c3ce8ecf7094f0a39d1733a1bdfe1e20c933858
lxhyl <zhangpengfan6@gmail.com>
2022-06-18 00:12:29 +0800

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { NotionAPI } from 'notion-client'
 
-type Data = {
-  name: string
-}
 
-export default function handler(
+import { idToUuid } from 'notion-utils'
+import { pageId } from '../../notion.config.json'
+const api = new NotionAPI()
+console.log("api", api)
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await api.getPage(pageId)
+  const collection_view = response.collection_view
+  const views = Object.values(collection_view)
+  const pages = views[0].value
+  // @ts-ignore
+  res.status(200).end(JSON.stringify({ sources: response, pages: pages.page_sort }))
 }
---
+++ pages/api/poem.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { NotionAPI } from "notion-client"
+const api = new NotionAPI()
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    const response = await api.getPage(req.query.id as string)
+
+    res.status(200).end(JSON.stringify({ response }))
+}
